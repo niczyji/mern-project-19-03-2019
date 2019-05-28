5cb7b4d4c4e8d4988c03607dcbdace44eeefb5cf
niczyji <niczyji@gmail.com>
2019-05-28 15:17:08 +0200

    bugs v2

--- client/src/components/posts/PostFeed.js
+++ client/src/components/posts/PostFeed.js
@@ -6,7 +6,7 @@ class PostFeed extends Component {
   render() {
     const { posts } = this.props;
 
-    return posts.maps(post => <PostItem key={post._id} post={post} />);
+    return posts.map(post => <PostItem key={post._id} post={post} />);
   }
 }
 
